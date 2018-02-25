94f04c4019ce46706e2d69871499e039dd2257ce
滑尧伟 <huayaowei@aliyun.com>
2018-02-25 19:20:01 +0800

    添加host

--- app/src/main/java/com/yunke/xiaovo/net/HRNetConfig.java
+++ app/src/main/java/com/yunke/xiaovo/net/HRNetConfig.java
@@ -7,12 +7,19 @@ public class HRNetConfig {
 
     public static final String SALT = "huarenkeji2018";
 
+//    public static final String HTTP = "http://";
+//    public static final String HOST = "47.94.44.114:8080/";
+//    public static final String URL = HTTP + HOST + "%s";
+//    public static final String PORKER_GAME_SOCKET_URL = "ws://47.94.44.114:8080/porkerGame/socket";
+
+
+
+
+
     public static final String HTTP = "http://";
-    public static final String HOST = "47.94.44.114:8080/";
+    public static final String HOST = "192.168.0.6:9090/";
     public static final String URL = HTTP + HOST + "%s";
-    public static final String PORKER_GAME_SOCKET_URL = "ws://47.94.44.114:8080/porkerGame/socket";
-
-
+    public static final String PORKER_GAME_SOCKET_URL = "ws://192.168.0.6:9090/porkerGame/socket";
 
 
 //    public static final String HTTP = "http://";
